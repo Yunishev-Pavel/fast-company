695e70f544428687b9b69129e82002865195ffe7
Pavel <112132913+Yunishev-Pavel@users.noreply.github.com>
2022-09-25 00:21:36 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Users from './components/users';
+// import Users from './components/users';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css"
+import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Users />
+    <App />
   </React.StrictMode>
 );
 
 
 reportWebVitals();
+
+
