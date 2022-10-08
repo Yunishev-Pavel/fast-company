2599ed10e688d874160a92c4286d46c314755bad
Pavel <112132913+Yunishev-Pavel@users.noreply.github.com>
2022-10-08 10:39:16 +0700

    Add files via upload

---
+++ user.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import BookMark from "./bookmark";
+import Quality from "./quality";
+import PropTypes from "prop-types";
+
+const User = ({
+  _id,
+  name,
+  qualities,
+  profession,
+  completedMeetings,
+  rate,
+  onDelete,
+  bookmark,
+  onToggleBookMark
+}) => {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>
+        {qualities.map((qual) => (
+          <Quality {...qual} key={qual._id} />
+        ))}
+      </td>
+      <td>{profession.name}</td>
+      <td>{completedMeetings}</td>
+      <td>{rate}/5</td>
+      <td>
+        <BookMark status={bookmark} onClick={() => onToggleBookMark(_id)} />
+      </td>
+      <td>
+        <button onClick={() => onDelete(_id)} className="btn btn-danger">
+          delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+User.propTypes = {
+  onToggleBookMark: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  qualities: PropTypes.array,
+  profession: PropTypes.object.isRequired,
+  completedMeetings: PropTypes.number.isRequired,
+  rate: PropTypes.number.isRequired,
+  bookmark: PropTypes.bool,
+  _id: PropTypes.string.isRequired
+};
+
+export default User;
