3471f43f33940b759bb8af5677d2a63086eaa85c
Pavel <112132913+Yunishev-Pavel@users.noreply.github.com>
2022-10-08 10:47:23 +0700

    Add files via upload

--- src/api/fake.api/user.api.js
+++ src/api/fake.api/user.api.js
@@ -1,4 +1,4 @@
-import { professions } from "./professions.api";
+import { professionsObject as professions } from "./professions.api";
 const qualities = {
   tedious: {
     _id: "67rdca3eeb7f6fgeed471198",
@@ -52,7 +52,7 @@ const users = [
     bookmark: false
   },
   {
-    id: "67rdca3eeb7f6fgeed471817",
+    _id: "67rdca3eeb7f6fgeed471817",
     name: "Боб Келсо",
     profession: professions.doctor,
     qualities: [qualities.buller],
@@ -142,6 +142,14 @@ const users = [
     bookmark: false
   }
 ];
-export function fetchAll() {
-  return users;
-}
+
+const fetchAll = () =>
+  new Promise((resolve) => {
+    window.setTimeout(function () {
+      resolve(users);
+    }, 2000);
+  });
+
+export default {
+  fetchAll
+};
