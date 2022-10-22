ce0de5836ed35547ec70245c8f5044a1a5705be5
Pavel <112132913+Yunishev-Pavel@users.noreply.github.com>
2022-10-22 13:10:02 +0700

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import React from "react";
-import Users from "./components/usersList";
+import Login from "./components/layouts/login";
+import Main from "./components/layouts/main";
+import NavBar from "./components/navBar";
+import Users from "./components/layouts/users";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
-  return <Users/>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/login" component={Login} />
+        <Route path="/users/:userId?" component={Users} />
+        <Redirect to="Loading" />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
