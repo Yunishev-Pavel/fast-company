5bb8a6c4c4ebdcde56f76023c17950f5bfa383c6
Pavel <112132913+Yunishev-Pavel@users.noreply.github.com>
2022-11-18 11:47:27 +0700

    Add files via upload

---
+++ src/components/common/table/tableHeader.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TableHeader = ({ onSort, selectedSort, columns }) => {
+    const handleSort = (item) => {
+        if (selectedSort.path === item) {
+            onSort({
+                ...selectedSort,
+                order: selectedSort.order === "asc" ? "desc" : "asc"
+            });
+        } else {
+            onSort({ path: item, order: "acs" });
+        }
+    };
+    const renderSortArrow = (selectedSort, currentPath) => {
+        if (selectedSort.path === currentPath) {
+            if (selectedSort.order === "asc") {
+                return <i className="bi bi-caret-down-fill"></i>;
+            } else {
+                return <i className="bi bi-caret-up-fill"></i>;
+            }
+        }
+        return null;
+    };
+
+    return (
+        <thead>
+            <tr>
+                {Object.keys(columns).map((colum) => (
+                    <th
+                        key={colum}
+                        onClick={
+                            columns[colum].path
+                                ? () => handleSort(columns[colum].path)
+                                : undefined
+                        }
+                        {...{ role: columns[colum].path && "button" }}
+                        scope="col"
+                    >
+                        {columns[colum].name}{" "}
+                        {renderSortArrow(selectedSort, columns[colum].path)}
+                    </th>
+                ))}
+            </tr>
+        </thead>
+    );
+};
+
+TableHeader.propTypes = {
+    onSort: PropTypes.func.isRequired,
+    selectedSort: PropTypes.object.isRequired,
+    columns: PropTypes.object.isRequired
+};
+
+export default TableHeader;
